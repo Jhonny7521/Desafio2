4cdaf72d59e3f15d82228403eacd86b41ab7b16d
Jhonny Quiliche <jhonny.jql1210@gmail.com>
2022-08-15 00:59:07 -0500

    ADD: Desafio2 - Parte 3 terminada

---
+++ 03/ejer1.sh
@@ -0,0 +1,18 @@
+#!/bin/echo
+
+text=""
+cd C:
+cd users/jhonny
+mkdir nueva_carpeta && cd nueva_carpeta
+touch archivo_nuevo.txt
+mv archivo_nuevo.txt archivo_viejo.txt
+
+echo -p "Ingrese texto" text
+echo $text >> archivo_viejo.txt
+
+chmod a=rwx archivo_viejo.txt
+
+cd ..
+ln -s nueva_carpeta/archivo_viejo.txt enlace.ln
+
+rm -r nueva_carpeta
